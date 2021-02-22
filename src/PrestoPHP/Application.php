dfe72f64e6b8e6148001857132e2ee569c6978f7
Gunnar Beushausen <info@gunnar-beushausen.de>
2021-02-22 04:22:58 +0100

    bumped version to 2.4.1-DEV

--- src/PrestoPHP/Application.php
+++ src/PrestoPHP/Application.php
@@ -42,7 +42,7 @@ use PrestoPHP\Provider\HttpKernelServiceProvider;
  */
 class Application extends Container implements HttpKernelInterface, TerminableInterface
 {
-    const VERSION = '2.4.0';
+    const VERSION = '2.4.1-DEV';
 
     const EARLY_EVENT = 512;
     const LATE_EVENT = -512;
